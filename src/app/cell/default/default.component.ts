1899a140cec826af75ec3d1a8b615236c909f5b5
Marco Lehmann <mlee@mlee.de>
2019-05-11 07:20:54 +0200

    Merge

--- src/app/cell/default/default.component.ts
+++ src/app/cell/default/default.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: '[app-default]',
   templateUrl: './default.component.html',
   styleUrls: ['./default.component.scss'],
