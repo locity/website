ad4e756cef36922a7004804c0cf858a77862b502
Marco Lehmann <mlee@mlee.de>
2019-10-25 16:46:52 +0200

    Fix problem with test.

--- src/app/cell/marco-steinhoff/marco-steinhoff.component.spec.ts
+++ src/app/cell/marco-steinhoff/marco-steinhoff.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { MarcoSteinhoffComponent } from './marco-steinhoff.component'
 
 
 describe('MarcoSteinhoffComponent', () => {
